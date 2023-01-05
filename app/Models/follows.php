58a1e748fbe9471d330ac24c6edf5f636d245451
Mohammedkhalil-Ali <muhamad.khalil12345@gmail.com>
2023-01-04 18:13:50 -0800

    ChangeModelName

--- app/Models/follows.php
+++ app/Models/follows.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class follows extends Model
+class Follows extends Model
 {
     use HasFactory;
 }
