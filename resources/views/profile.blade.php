e7da524babf799b984f51a20af85a8654e9e7c59
Mohammedkhalil-Ali <muhamad.khalil12345@gmail.com>
2023-01-07 19:23:15 +0300

    6

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -177,7 +177,7 @@
             </div>
         </div>
     </div>
-{{-- 9876 --}}
+{{-- 987 --}}
     <script>
         const btn=document.querySelector("#sharepost");
         let img=document.querySelector("#img");
